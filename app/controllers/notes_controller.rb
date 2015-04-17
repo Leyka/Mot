67dfe673c78b9f3e089483436d7af07e3e9a9c33
Skander <skander.kc@gmaill.com>
2015-04-16 23:18:15 -0400

    Set up notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,31 @@
 class NotesController < ApplicationController
+  before_action :set_note, only: [:update, :edit, :show]
+
   def index
+  end
+
+  def new 
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  private
+
+  def set_note
+    @note = Note.find(params[:id])
+  end
+
+  def note_params
+    params.require(:note).permit(:title, :body)
   end
 end
